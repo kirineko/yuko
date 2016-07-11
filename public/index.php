3ce8189ee1a643541884e6b2df2c80d6e29a1bea
kirineko <kirinekoneko@gmail.com>
2016-07-12 03:16:51 +0800

    add some yuko init file

--- public/index.php
+++ public/index.php
@@ -32,7 +32,7 @@ require __DIR__.'/../bootstrap/autoload.php';
 | the responses back to the browser and delight our users.
 |
 */
-phpinfo();die;
+
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
 /*
